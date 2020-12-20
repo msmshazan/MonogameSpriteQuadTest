9d826a5420e033fa0994fdbe96d63b2be2b1d5fb
mohamed shazan <msmshazan1@gmail.com>
2020-12-20 16:57:11 +0530

    Initial Commit

---
+++ QuadTest/Content/SpriteShader.fx
@@ -0,0 +1,61 @@
+﻿#include "Macros.fxh"
+
+#define VS_SHADERMODEL vs_4_0
+#define PS_SHADERMODEL ps_4_0
+
+static float2 corners[4] = { float2(-1,1) , float2(1,1) ,
+					float2(-1,-1) , float2(1,-1) };
+
+BEGIN_CONSTANTS
+MATRIX_CONSTANTS
+float4x4 MatrixTransform    _vs(c0) _cb(c0);
+float4 TextureDim;
+END_CONSTANTS
+
+
+DECLARE_TEXTURE(Texture, 0);
+
+
+struct VertexShaderInput
+{
+	float3 Pos : POSITION0;
+	float2 Dim : POSITION1;
+	float2 Scale : POSITION2;
+	float2 UV : TEXCOORD0;
+	float4 Color	: COLOR0;
+};
+
+struct VertexShaderOutput
+{
+	float4 Position : SV_POSITION;
+	float4 Color : COLOR0;
+	float2 UV : TEXCOORD0;
+};
+
+VertexShaderOutput MainVS(in VertexShaderInput input, uint id:SV_VERTEXID)
+{
+	VertexShaderOutput output = (VertexShaderOutput)0;
+	int index = (int)id;
+	float2 corner = corners[index % 4];
+	float2 pos = input.Pos.xy + (input.Dim*input.Scale*corner);
+	output.Position = mul(float4(pos.x,pos.y, input.Pos.z,1), MatrixTransform);
+	output.UV =  (input.UV + (input.Dim * corner) )*TextureDim.zw;
+	output.Color = input.Color;
+	return output;
+}
+
+float4 MainPS(VertexShaderOutput input) : COLOR
+{
+	float2 uv = input.UV;
+	float4 texel = SAMPLE_TEXTURE(Texture, uv);
+	return texel;
+}
+
+technique AnimationRendering
+{
+	pass P0
+	{
+		VertexShader = compile VS_SHADERMODEL MainVS();
+		PixelShader = compile PS_SHADERMODEL MainPS();
+	}
+};
---
+++ QuadTest/SpriteVertex.cs
@@ -0,0 +1,89 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Xna.Framework;
+using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace QuadTest
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct SpriteVertex : IVertexType
+    {
+        public Vector3 Pos;
+        public Vector2 Dim;
+        public Vector2 Scale;
+        public Vector2 UV;
+        public Color Color;
+        public static readonly VertexDeclaration VertexDeclaration;
+        public SpriteVertex(Vector3 position,Vector2 dimension,Vector2 uv, Vector2 scale,Color color)
+        {
+            Scale = scale;
+            Pos = position;
+            Dim = dimension;
+            UV = uv;
+            Color = color;
+        }
+
+        VertexDeclaration IVertexType.VertexDeclaration
+        {
+            get { return VertexDeclaration; }
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Pos.GetHashCode();
+                hashCode = (hashCode * 397) ^ Dim.GetHashCode();
+                hashCode = (hashCode * 397) ^ Scale.GetHashCode();
+                hashCode = (hashCode * 397) ^ UV.GetHashCode();
+                hashCode = (hashCode * 397) ^ Color.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Pos: {Pos} , UV : {UV} , Scale: {Scale},Dim : {Dim},Color : {Color}";
+        }
+
+        public static bool operator ==(SpriteVertex left, SpriteVertex right)
+        {
+            return left.Pos == right.Pos
+                && left.Dim == right.Dim
+                && left.Scale == right.Scale
+                && left.UV == right.UV
+                && left.Color == right.Color;
+        }
+
+        public static bool operator !=(SpriteVertex left, SpriteVertex right)
+        {
+            return !(left == right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+            return this == (SpriteVertex)obj;
+        }
+
+        static SpriteVertex()
+        {
+            var elements = new VertexElement[]
+            {
+                new VertexElement(0, VertexElementFormat.Vector3, VertexElementUsage.Position, 0),
+                new VertexElement(12, VertexElementFormat.Vector2, VertexElementUsage.Position, 1),
+                new VertexElement(20, VertexElementFormat.Vector2, VertexElementUsage.Position, 2),
+                new VertexElement(28, VertexElementFormat.Vector2, VertexElementUsage.TextureCoordinate, 0),
+                new VertexElement(36, VertexElementFormat.Color, VertexElementUsage.Color, 0)
+            };
+            VertexDeclaration = new VertexDeclaration(elements);
+        }
+    }
+}
